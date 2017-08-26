76da4d1fea837b662abef3ba61a813a5f7f6c4bf
MrInternauta <mr.internauta@hotmail.com>
2017-08-25 23:51:21 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Calculadora</title>
+  </head>
+  <body>
+    <form action="pagina2.php" method="post">
+      <input type="text" name="n1">
+      <select class="" name="opciones">
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+      </select>
+      <input type="text" name="n2">
+      <input type="submit"  value="Calcular">
+    </form>
+  </body>
+</html>
